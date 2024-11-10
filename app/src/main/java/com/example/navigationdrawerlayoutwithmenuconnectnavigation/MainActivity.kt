4c59d925a749dfb9aad9618154239061d3aed6e2
Mrbexruz <vitusbehruzhp@gmail.com>
2024-11-10 21:36:49 +0500

    Navigation Drawer layout , Experience yani osonroq va clean code

--- app/src/main/java/com/example/navigationdrawerlayoutwithmenuconnectnavigation/MainActivity.kt
+++ app/src/main/java/com/example/navigationdrawerlayoutwithmenuconnectnavigation/MainActivity.kt
@@ -5,6 +5,8 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.example.navigationdrawerlayoutwithmenuconnectnavigation.databinding.ActivityMainBinding
 import com.example.navigationdrawerlayoutwithmenuconnectnavigation.fragments.HomeFragment
 import com.example.navigationdrawerlayoutwithmenuconnectnavigation.fragments.SettingsFragment
@@ -18,22 +20,7 @@ class MainActivity : AppCompatActivity() {
         val homeFragment = HomeFragment()
         val settingsFragment = SettingsFragment()
 
-        supportFragmentManager.beginTransaction()
-            .add(R.id.my_container, homeFragment)
-            .commit()
-
-        binding.myNavigationView.setNavigationItemSelectedListener {
-            when(it.itemId){
-                R.id.menu_home ->{
-                    supportFragmentManager.beginTransaction().replace(R.id.my_container, homeFragment).commit()
-
-                }
-                R.id.menu_settings ->{
-                    supportFragmentManager.beginTransaction().replace(R.id.my_container, settingsFragment).commit()
-                }
-            }
-            binding.myDrawerLayout.closeDrawers()
-            true
-        }
+        val navController = findNavController(R.id.my_navigation_host)
+        binding.myNavigationView.setupWithNavController(navController)
     }
 }
